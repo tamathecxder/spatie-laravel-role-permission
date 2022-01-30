f4d1a0a190001c38e340238fba29698424ecd804
deezycxde <yudistira@smkn1cianjur.sch.id>
2022-01-30 10:56:08 +0700

    First commit and assign new roles for user and admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin Role',
+            'email' => 'admin@role.test',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'User Role',
+            'email' => 'user@role.test',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user->assignRole('user');
+    }
+}
